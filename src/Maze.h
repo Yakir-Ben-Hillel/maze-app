d202cd520488b2ae834b6f98332c94217ab87c6c
MBM9 <maor9920@gmail.com>
2019-11-30 22:50:11 +0200

    Update Maze.h

--- src/Maze.h
+++ src/Maze.h
@@ -11,11 +11,12 @@ public:
 Maze(int h=max_h,int w=max_w);
 Maze(Maze& other)=default;
 ~Maze();
-void createMaze(int h,int w);
+void createMaze();
 const char** getMaze() const;
 bool ifValidMaze(char** maze,int h,int w);
 int gethight() const;
 int getwidth() const;
+bool hasNeighbors(int curr_h,int curr_w);
 private:
 char** maze;
 int h,w;
