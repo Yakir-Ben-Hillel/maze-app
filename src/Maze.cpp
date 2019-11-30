351a78c05df764608f56899fbda31de0c43afd1c
MBM9 <maor9920@gmail.com>
2019-11-30 22:50:13 +0200

    Update Maze.cpp

--- src/Maze.cpp
+++ src/Maze.cpp
@@ -38,3 +38,27 @@ int Maze::getwidth() const
 {
     return this->w;
 }
+
+void Maze::createMaze()
+{
+    //in the constractor we make the full maze so,we already know that the member from the main who call this function is a full maze.
+    Stack s;
+    int curr_h,curr_w;
+    s.push(1,1,'*');//setting the stack to the first element in the maze.
+    while(!s.isEmpty)
+    {
+        Node* temp;
+        temp=s.pop();
+        temp->getData(curr_h,curr_w);//updating the curr_h and curr_w by ref
+        this->maze[curr_h][curr_w]=' ';//mark the point we popped from the stack
+        if(hasNeighbors(curr_h,curr_w))
+        {
+
+        }
+    }
+}
+
+bool Maze::hasNeighbors(int curr_h,int curr_w)
+{
+
+}
