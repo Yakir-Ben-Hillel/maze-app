1ecbcb37ca6ee65e070a612798efab086970f8cc
MBM9 <maor9920@gmail.com>
2019-12-05 15:15:30 +0200

    x

--- src/classes/Maze.cpp
+++ src/classes/Maze.cpp
@@ -32,7 +32,7 @@ void Maze::createMaze()
 {
     //in the constractor we make the full maze so,we already know that the member from the main who call this function is a full maze.
     Stack s;
-    int curr_h, curr_w,neighbor_h,neighbor_w;
+    int curr_h, curr_w, neighbor_h, neighbor_w;
     s.push(1, 1, ' '); //setting the stack to the first element in the maze.
     while (!s.isEmpty)
     {
@@ -40,47 +40,47 @@ void Maze::createMaze()
         temp = s.pop();
         temp->getData(curr_h, curr_w);    //updating the curr_h and curr_w by ref
         this->maze[curr_h][curr_w] = ' '; //mark the point we popped from the stack
-        if (hasNeighbors(curr_h, curr_w,neighbor_h,neighbor_w))
+        if (hasNeighbors(curr_h, curr_w, neighbor_h, neighbor_w))
         {
-            s.push(curr_h,curr_w,' ');
-            s.push(neighbor_h,neighbor_w,'$');
+            s.push(curr_h, curr_w, ' ');
+            s.push(neighbor_h, neighbor_w, '$');
         }
     }
 }
 
-bool Maze::hasNeighbors(int curr_h, int curr_w,int& neighbor_h,int& neighbor_w) 
+bool Maze::hasNeighbors(int curr_h, int curr_w, int &neighbor_h, int &neighbor_w)
 {
     srand((unsigned)time(NULL));
-    int x=rand();
-    if(x%4==0&&curr_h-2>0&&this->getMaze[curr_h-2][curr_w]!='$')
+    int x = rand();
+    if (x % 4 == 0 && curr_h - 2 > 0 && this->getMaze[curr_h - 2][curr_w] != '$')
     {
-        neighbor_h=curr_h-2;
-        neighbor_w=curr_w;
-        this->maze[curr_h-1][curr_w]=' ';//breaking the wall between the neighbors
+        neighbor_h = curr_h - 2;
+        neighbor_w = curr_w;
+        this->maze[curr_h - 1][curr_w] = ' '; //breaking the wall between the neighbors
         return true;
     }
-    else if(x%4==1&&curr_w-2>0&&this->maze[curr_h][curr_w-2]!='$')
+    else if (x % 4 == 1 && curr_w - 2 > 0 && this->maze[curr_h][curr_w - 2] != '$')
     {
-        neighbor_h=curr_h;
-        neighbor_w=curr_w-2;
-        this->maze[curr_h][curr_w-1]=' ';//breaking the wall between the neighbors
+        neighbor_h = curr_h;
+        neighbor_w = curr_w - 2;
+        this->maze[curr_h][curr_w - 1] = ' '; //breaking the wall between the neighbors
         return true;
     }
-    else if(x%4==2&&curr_h+2<this->h-1&&this->maze[curr_h+2][curr_w]!='$')
+    else if (x % 4 == 2 && curr_h + 2 < this->h - 1 && this->maze[curr_h + 2][curr_w] != '$')
     {
-        neighbor_h=curr_h+2;
-        neighbor_w=curr_w;
-        this->maze[curr_h+1][curr_w]=' ';//breaking the wall between the neighbors
+        neighbor_h = curr_h + 2;
+        neighbor_w = curr_w;
+        this->maze[curr_h + 1][curr_w] = ' '; //breaking the wall between the neighbors
         return true;
     }
-    else if(x%4==3&&curr_w+2<this->w-1&&this->maze[curr_h][curr_w+2]!='$')
+    else if (x % 4 == 3 && curr_w + 2 < this->w - 1 && this->maze[curr_h][curr_w + 2] != '$')
     {
-        neighbor_h=curr_h;
-        neighbor_w=curr_w+2;
-        this->maze[curr_h][curr_w+1]=' ';//breaking the wall between the neighbors
+        neighbor_h = curr_h;
+        neighbor_w = curr_w + 2;
+        this->maze[curr_h][curr_w + 1] = ' '; //breaking the wall between the neighbors
         return true;
     }
-    return false;//if non of the ifs had happened the current point in the maze has no neighbors
+    return false; //if non of the ifs had happened the current point in the maze has no neighbors
 }
 
 void Maze::setMaze(char **maze)
@@ -166,6 +166,6 @@ bool ifValidMaze(char **maze, int h, int w)
                     return false;
             }
         }
+        return true; //if all the checks didn't give false the maze is valid.
     }
-    return true;//if all the checks didn't give false the maze is valid.
 }
