eb809733d5dd651660a968725c5d2034e2ac45fb
MBM9 <maor9920@gmail.com>
2019-12-06 09:39:45 +0200

    g

--- src/classes/Maze.cpp
+++ src/classes/Maze.cpp
@@ -41,7 +41,7 @@ void Maze::createMaze()
         temp->getData(curr_h, curr_w); //updating the curr_h and curr_w by ref
         if (hasNeighbors(curr_h, curr_w, neighbor_h, neighbor_w, s))
         {
-            s.push(curr_h, curr_w, ' ');
+            s.push(curr_h, curr_w, '$');
             s.push(neighbor_h, neighbor_w, ' '); //mark the neighbor to know that we visit there already.
             //Staging to next block.
         }
